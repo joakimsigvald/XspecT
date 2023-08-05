363c36d8009043c1c7ae59e9c9841b5fb0822968
Joakim Sigvald <joakim.sigvald@adlibris.com>
2023-08-05 21:52:00 +0200

    ...

--- Core.Test/Subjects/Shopping/Basket.cs
+++ Core.Test/Subjects/Shopping/Basket.cs
@@ -3,6 +3,7 @@
 public class Basket
 {
     public int Id { get; set; }
+    public BasketItem[] Items { get; internal set; }
     public int CustomerId { get; set; }
     public int CompanyId { get; set; }
 }
