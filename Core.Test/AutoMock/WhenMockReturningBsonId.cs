f960086d7641a49c96878a4d8cac448364721171
Joakim Sigvald <joakim.sigvald@adlibris.com>
2023-08-20 21:10:29 +0200

    Fallback to Activator.CreateInstance when AutoFixture fail to create value

---
+++ Core.Test/AutoMock/WhenMockReturningBsonId.cs
@@ -0,0 +1,29 @@
+﻿using XspecT.Fixture;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using XspecT.Verification;
+
+namespace XspecT.Test.AutoMock;
+
+public class WhenGetRecordWithBsonIdFromMock : SubjectSpec<BsonIdService, RecordMongoDb>
+{
+    public WhenGetRecordWithBsonIdFromMock() => When(_ => _.GetRecord());
+    [Fact] public void ThenGetRecord() => Then().Result.Is().NotNull();
+}
+
+public class BsonIdService
+{
+    private readonly IBsonIdRepository _repo;
+    public BsonIdService(IBsonIdRepository repo) => _repo = repo;
+    public Task<RecordMongoDb> GetRecord() => _repo.GetRecord();
+}
+
+public interface IBsonIdRepository
+{
+    Task<RecordMongoDb> GetRecord();
+}
+
+public class RecordMongoDb
+{
+    [BsonId] public ObjectId Id { get; set; }
+}
--- Core/Internal/Context.cs
+++ Core/Internal/Context.cs
@@ -29,7 +29,7 @@ internal class Context
     internal TValue Mention<TValue>(TValue value, int index = 0)
         => (GetMentions(typeof(TValue))[index] = value) is TValue v ? v : default;
 
-    internal TValue Create<TValue>() 
+    internal TValue Create<TValue>()
         => typeof(TValue).IsInterface ? _mocker.Get<TValue>() : _fixture.Create<TValue>();
 
     internal object CreateDefaultValue(Type type)
@@ -38,9 +38,16 @@ internal class Context
         {
             return _fixture.Create(type, new SpecimenContext(_fixture));
         }
-        catch (ObjectCreationException oce)
+        catch (Exception ex)
         {
-            throw new SetupFailed($"Failed to create value for type {type.Name}", oce);
+            try
+            {
+                return Activator.CreateInstance(type);
+            }
+            catch
+            {
+                throw new SetupFailed($"Failed to create value for type {type.Name}", ex);
+            }
         }
     }
 
@@ -48,8 +55,8 @@ internal class Context
         => _mentions.TryGetValue(type, out var val) ? val : _mentions[type] = new object[5];
 
     private object Retreive(Type type, int index = 0)
-        => _mentions.TryGetValue(type, out var vals) ? vals[index] 
-        : index == 0 && _usings.TryGetValue(type, out var val) ? val 
+        => _mentions.TryGetValue(type, out var vals) ? vals[index]
+        : index == 0 && _usings.TryGetValue(type, out var val) ? val
         : null;
 
     private static IFixture CreateAutoFixture()
