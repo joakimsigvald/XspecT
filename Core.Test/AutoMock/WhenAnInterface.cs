9dd70c52fb6f2d5e9bd7734594f940662c8d61f3
Joakim Sigvald <joakim.sigvald@adlibris.com>
2023-10-27 17:44:17 +0200

    added green test

--- Core.Test/AutoMock/WhenAnInterface.cs
+++ Core.Test/AutoMock/WhenAnInterface.cs
@@ -5,11 +5,20 @@ namespace XspecT.Test.AutoMock;
 
 public class WhenInjectingAnInterfaceWithUsing : SubjectSpec<InterfaceService, int>
 {
-    public WhenInjectingAnInterfaceWithUsing() 
+    public WhenInjectingAnInterfaceWithUsing()
         => Using(new MyComponent(An<IMyLogger>(), An<int>())).When(_ => _.GetValue());
 
     [Fact] public void ThenGetValue() => Result.Is(The<int>());
     [Fact] public void ThenInterfaceIsMocked() => Then<IMyLogger>(_ => _.LogValue(The<int>()));
+}
+
+public class WhenUsingConcreteInstanceForInterface : SubjectSpec<InterfaceService, int>
+{
+    public WhenUsingConcreteInstanceForInterface()
+        => When(_ => _.GetValue()).Using(() => new MyComponent(An<IMyLogger>(), An<int>()))
+        .And<IMyLogger>(() => new MyInvalidLogger());
+
+    [Fact] public void ThenUseTheConcreteInstance() => Then().Throws<InvalidOperationException>();
 }
 
 public class InterfaceService
@@ -42,4 +51,9 @@ public class MyComponent
 public interface IMyLogger
 {
     void LogValue(int value);
+}
+
+public class MyInvalidLogger : IMyLogger
+{
+    public void LogValue(int value) => throw new InvalidOperationException();
 }
