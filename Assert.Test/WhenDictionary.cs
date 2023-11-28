1743790843f15a142e553d2b54174b94fbacd6dc
Joakim Sigvald <joakim.sigvald@adlibris.com>
2023-11-28 21:17:00 +0100

    Updated all packages to latest versions added testcases to Assert

---
+++ Assert.Test/WhenDictionary.cs
@@ -0,0 +1,16 @@
+﻿using XspecT.Assert;
+using Xunit;
+
+namespace XspecT.Test.Verification;
+
+public class WhenDictionary : StaticSpec<Dictionary<string, int>>
+{
+    [Fact]
+    public void IsEqual()
+    {
+        var pairs = new[] { ("a", 1), ("b", 2) };
+        var dict1 = pairs.ToDictionary(p => p.Item1, p => p.Item2);
+        var dict2 = pairs.ToDictionary(p => p.Item1, p => p.Item2);
+        dict1.Is().EqualTo(dict2);
+    }
+}
---
+++ Assert.Test/WhenList.cs
@@ -0,0 +1,16 @@
+﻿using XspecT.Assert;
+using Xunit;
+
+namespace XspecT.Test.Verification;
+
+public class WhenList : StaticSpec<List<string>>
+{
+    [Fact]
+    public void IsEqual()
+    {
+        var values = new[] { "a", "b" };
+        var list1 = values.ToList();
+        var list2 = values.ToList();
+        list1.Is().EqualTo(list2);
+    }
+}
