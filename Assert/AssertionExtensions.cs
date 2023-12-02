50eefeb94bdb709e903f7ca7c72fffdececc96e9
Joakim Sigvald <joakim.sigvald@adlibris.com>
2023-12-02 22:13:24 +0100

    Added documentation on AssertionExtensions

--- Assert/AssertionExtensions.cs
+++ Assert/AssertionExtensions.cs
@@ -5,208 +5,208 @@ using XspecT.Assert.Time;
 namespace XspecT.Assert;
 
 /// <summary>
-/// TODO
+/// Fluent assertions with verbs Is, Has and Does
 /// </summary>
 public static class AssertionExtensions
 {
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given value
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsByte Is(this byte actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given value
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsSByte Is(this sbyte actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given value
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsShort Is(this short actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given value
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsUShort Is(this ushort actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given value
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsInt Is(this int actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given value
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsUInt Is(this uint actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given value
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsLong Is(this long actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given value
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsULong Is(this ulong actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given value
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsFloat Is(this float actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given value
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsDouble Is(this double actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given value
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsDecimal Is(this decimal actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given value
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsNullableByte Is(this byte? actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given value
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsNullableSByte Is(this sbyte? actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given value
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsNullableShort Is(this short? actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given value
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsNullableUShort Is(this ushort? actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given value
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsNullableInt Is(this int? actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given value
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsNullableUInt Is(this uint? actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given value
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsNullableLong Is(this long? actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given value
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsNullableULong Is(this ulong? actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given value
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsNullableFloat Is(this float? actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given value
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsNullableDouble Is(this double? actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given value
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsNullableDecimal Is(this decimal? actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given value
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsBool Is(this bool actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given value
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsDateTime Is(this DateTime actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given value
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsTimeSpan Is(this TimeSpan actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given string
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsString Is(this string actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the characteristics of the given string
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static DoesString Does(this string actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given object
     /// </summary>
     /// <param name="actual"></param>
     /// <returns></returns>
     public static IsObject Is(this object actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given enumerable
     /// </summary>
     /// <typeparam name="TItem"></typeparam>
     /// <param name="actual"></param>
@@ -214,7 +214,7 @@ public static class AssertionExtensions
     public static IsEnumerable<TItem> Is<TItem>(this IEnumerable<TItem> actual) => new(actual);
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for the given comparable
     /// </summary>
     /// <typeparam name="TValue"></typeparam>
     /// <param name="actual"></param>
@@ -224,7 +224,7 @@ public static class AssertionExtensions
         => new(actual);
 
     /// <summary>
-    /// actual.Should().Be(expected)
+    /// Verify that actual is expected and return continuation for further assertions of the value
     /// </summary>
     public static ContinueWith<IsNullableByte> Is(this byte? actual, byte? expected)
     {
@@ -233,7 +233,7 @@ public static class AssertionExtensions
     }
 
     /// <summary>
-    /// actual.Should().Be(expected)
+    /// Verify that actual is expected and return continuation for further assertions of the value
     /// </summary>
     public static ContinueWith<IsNullableSByte> Is(this sbyte? actual, sbyte? expected)
     {
@@ -242,7 +242,7 @@ public static class AssertionExtensions
     }
 
     /// <summary>
-    /// actual.Should().Be(expected)
+    /// Verify that actual is expected and return continuation for further assertions of the value
     /// </summary>
     public static ContinueWith<IsNullableUShort> Is(this ushort? actual, ushort? expected)
     {
@@ -251,7 +251,7 @@ public static class AssertionExtensions
     }
 
     /// <summary>
-    /// actual.Should().Be(expected)
+    /// Verify that actual is expected and return continuation for further assertions of the value
     /// </summary>
     public static ContinueWith<IsNullableShort> Is(this short? actual, short? expected)
     {
@@ -260,7 +260,7 @@ public static class AssertionExtensions
     }
 
     /// <summary>
-    /// actual.Should().Be(expected)
+    /// Verify that actual is expected and return continuation for further assertions of the value
     /// </summary>
     public static ContinueWith<IsNullableUInt> Is(this uint? actual, uint? expected)
     {
@@ -269,7 +269,7 @@ public static class AssertionExtensions
     }
 
     /// <summary>
-    /// actual.Should().Be(expected)
+    /// Verify that actual is expected and return continuation for further assertions of the value
     /// </summary>
     public static ContinueWith<IsNullableInt> Is(this int? actual, int? expected)
     {
@@ -278,7 +278,7 @@ public static class AssertionExtensions
     }
 
     /// <summary>
-    /// actual.Should().Be(expected)
+    /// Verify that actual is expected and return continuation for further assertions of the value
     /// </summary>
     public static ContinueWith<IsNullableULong> Is(this ulong? actual, ulong? expected)
     {
@@ -287,7 +287,7 @@ public static class AssertionExtensions
     }
 
     /// <summary>
-    /// actual.Should().Be(expected)
+    /// Verify that actual is expected and return continuation for further assertions of the value
     /// </summary>
     public static ContinueWith<IsNullableLong> Is(this long? actual, long? expected)
     {
@@ -296,7 +296,7 @@ public static class AssertionExtensions
     }
 
     /// <summary>
-    /// actual.Should().Be(expected)
+    /// Verify that actual is expected and return continuation for further assertions of the value
     /// </summary>
     public static ContinueWith<IsNullableFloat> Is(this float? actual, float? expected)
     {
@@ -305,7 +305,7 @@ public static class AssertionExtensions
     }
 
     /// <summary>
-    /// actual.Should().Be(expected)
+    /// Verify that actual is expected and return continuation for further assertions of the value
     /// </summary>
     public static ContinueWith<IsNullableDouble> Is(this double? actual, double? expected)
     {
@@ -314,7 +314,7 @@ public static class AssertionExtensions
     }
 
     /// <summary>
-    /// actual.Should().Be(expected)
+    /// Verify that actual is expected and return continuation for further assertions of the value
     /// </summary>
     public static ContinueWith<IsNullableDecimal> Is(this decimal? actual, decimal? expected)
     {
@@ -323,7 +323,7 @@ public static class AssertionExtensions
     }
 
     /// <summary>
-    /// TODO
+    /// Verify that actual is expected timeSpan and return continuation for further assertions of the value
     /// </summary>
     /// <param name="actual"></param>
     /// <param name="expected"></param>
@@ -335,7 +335,7 @@ public static class AssertionExtensions
     }
 
     /// <summary>
-    /// TODO
+    /// Verify that actual is expected timeSpan and return continuation for further assertions of the value
     /// </summary>
     /// <param name="actual"></param>
     /// <param name="expected"></param>
@@ -347,7 +347,7 @@ public static class AssertionExtensions
     }
 
     /// <summary>
-    /// TODO
+    /// Verify that actual is expected dateTime and return continuation for further assertions of the value
     /// </summary>
     /// <param name="actual"></param>
     /// <param name="expected"></param>
@@ -359,7 +359,7 @@ public static class AssertionExtensions
     }
 
     /// <summary>
-    /// TODO
+    /// Verify that actual is expected dateTime and return continuation for further assertions of the value
     /// </summary>
     /// <param name="actual"></param>
     /// <param name="expected"></param>
@@ -371,7 +371,7 @@ public static class AssertionExtensions
     }
 
     /// <summary>
-    /// actual.Should().BeSameAs(expected)
+    /// Verify that actual object is same reference as expected and return continuation for further assertions of the object
     /// </summary>
     public static ContinueWith<IsObject> Is(this object actual, object expected)
     {
@@ -380,7 +380,7 @@ public static class AssertionExtensions
     }
 
     /// <summary>
-    /// TODO
+    /// Get available assertions for enumerable
     /// </summary>
     /// <typeparam name="TItem"></typeparam>
     /// <param name="actual"></param>
@@ -388,7 +388,7 @@ public static class AssertionExtensions
     public static HasEnumerable<TItem> Has<TItem>(this IEnumerable<TItem> actual) => new(actual);
 
     /// <summary>
-    /// actual.Should().Be(expected)
+    /// Verify that actual struct is same as expected and return continuation for further assertions of the struct
     /// </summary>
     public static ContinueWith<IsObject> Is<TValue>(this TValue actual, TValue expected) where TValue : struct
     {
@@ -397,7 +397,7 @@ public static class AssertionExtensions
     }
 
     /// <summary>
-    /// actual.Should().Be(expected)
+    /// Verify that actual string is same as expected and return continuation for further assertions of the string
     /// </summary>
     public static ContinueWith<IsString> Is(this string actual, string expected)
     {
@@ -406,12 +406,12 @@ public static class AssertionExtensions
     }
 
     /// <summary>
-    /// TODO
+    /// Provide actual of any type to continue the chain of assertions on the new value
     /// </summary>
-    /// <typeparam name="TService"></typeparam>
+    /// <typeparam name="TActual"></typeparam>
     /// <typeparam name="TContinuation"></typeparam>
     /// <param name="_"></param>
-    /// <param name="service"></param>
+    /// <param name="actual"></param>
     /// <returns></returns>
-    public static TService And<TService, TContinuation>(this ContinueWith<TContinuation> _, TService service) => service;
+    public static TActual And<TActual, TContinuation>(this ContinueWith<TContinuation> _, TActual actual) => actual;
 }
