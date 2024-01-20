d69c8ca6911f03d3ca568e94017ca9b0bf5ffc30
Joakim Sigvald <joakim.sigvald@adlibris.com>
2024-01-20 14:33:51 +0100

    Removed irrelevant methods on static test pipeline and document remaining public methods

--- Core/Internal/Pipelines/StaticTestPipeline.cs
+++ Core/Internal/Pipelines/StaticTestPipeline.cs
@@ -11,12 +11,6 @@ internal class StaticTestPipeline<TValue, TResult>
     internal StaticTestPipeline(StaticSpec<TResult> parent)
         : base(parent) { }
 
-    public IStaticTestPipeline<TValue, TResult> Given(TValue value)
-        => Parent.Given(value);
-
-    public IStaticTestPipeline<TValue, TResult> When(Action<TValue> act)
-        => Parent.When(act);
-
     public IStaticTestPipeline<TValue, TResult> When(Func<TValue, TResult> act)
         => Parent.When(act);
 
@@ -33,12 +27,6 @@ internal class StaticTestPipeline<TValue1, TValue2, TResult>
     internal StaticTestPipeline(StaticSpec<TResult> parent)
         : base(parent) { }
 
-    public IStaticTestPipeline<TValue1, TValue2, TResult> Given(TValue1 value1, TValue2 value2)
-        => Parent.Given(value1, value2);
-
-    public IStaticTestPipeline<TValue1, TValue2, TResult> When(Action<TValue1, TValue2> act)
-        => Parent.When(act);
-
     public IStaticTestPipeline<TValue1, TValue2, TResult> When(Func<TValue1, TValue2, TResult> act)
         => Parent.When(act);
 
@@ -54,12 +42,6 @@ internal class StaticTestPipeline<TValue1, TValue2, TValue3, TResult>
 {
     internal StaticTestPipeline(StaticSpec<TResult> parent)
         : base(parent) { }
-
-    public IStaticTestPipeline<TValue1, TValue2, TValue3, TResult> Given(TValue1 value1, TValue2 value2, TValue3 value3)
-        => Parent.Given(value1, value2, value3);
-
-    public IStaticTestPipeline<TValue1, TValue2, TValue3, TResult> When(Action<TValue1, TValue2, TValue3> act)
-        => Parent.When(act);
 
     public IStaticTestPipeline<TValue1, TValue2, TValue3, TResult> When(Func<TValue1, TValue2, TValue3, TResult> act)
         => Parent.When(act);
