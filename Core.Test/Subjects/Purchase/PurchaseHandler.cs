1f16456a0d49f70b50ce53906eee54f254a18049
Joakim Sigvald <joakim.sigvald@adlibris.com>
2024-06-29 20:29:32 +0200

    ...

--- Core.Test/Subjects/Purchase/PurchaseHandler.cs
+++ Core.Test/Subjects/Purchase/PurchaseHandler.cs
@@ -1,28 +1,17 @@
 ﻿namespace XspecT.Test.Subjects.Purchase;
 
-public class PurchaseHandler
+public class PurchaseHandler(
+    IBasketRepository basketRepository,
+    ICheckoutProvider checkoutProvider,
+    ITopicExchangeV2<BasketPurchasedV1> basketPurchased)
 {
-    private readonly IBasketRepository _basketRepository;
-    private readonly ICheckoutProvider _checkoutProvider;
-    private readonly ITopicExchangeV2<BasketPurchasedV1> _basketPurchased;
-
-    public PurchaseHandler(
-        IBasketRepository basketRepository,
-        ICheckoutProvider checkoutProvider,
-        ITopicExchangeV2<BasketPurchasedV1> basketPurchased)
-    {
-        _basketRepository = basketRepository;
-        _checkoutProvider = checkoutProvider;
-        _basketPurchased = basketPurchased;
-    }
-
     public async Task<PurchaseResponseModel> Purchase(int basketId)
     {
-        var checkout = await _checkoutProvider.GetExistingCheckout(basketId);
+        var checkout = await checkoutProvider.GetExistingCheckout(basketId);
         checkout.IsOpen = false;
-        await _basketRepository.UpdateStatus(basketId);
+        await basketRepository.UpdateStatus(basketId);
         var ev = new BasketPurchasedV1();
-        await _basketPurchased.Publish(ev);
+        await basketPurchased.Publish(ev);
         return new PurchaseResponseModel();
     }
 }
--- Core.Test/Subjects/ShoppingService.cs
+++ Core.Test/Subjects/ShoppingService.cs
@@ -1,32 +1,20 @@
 ﻿namespace XspecT.Test.Subjects;
 
-public class ShoppingService
+public class ShoppingService(
+    IOrderService orderService,
+    ILogger logger,
+    (string shop, string division) names,
+    int shopId)
 {
-    private readonly IOrderService _orderService;
-    private readonly ILogger _logger;
-    private readonly int _shopId;
-    private readonly string _shopName;
-    private readonly string _division;
-
-    public ShoppingService(
-        IOrderService orderService,
-        ILogger logger,
-        (string shop, string division) names,
-        int shopId)
-    {
-        _orderService = orderService;
-        _logger = logger;
-        _shopId = shopId;
-        _shopName = names.shop;
-        _division = names.division;
-    }
+    private readonly string _shopName = names.shop;
+    private readonly string _division = names.division;
 
     public ShoppingCart CreateCart(int id) => new() { Id = id };
 
     public void PlaceOrder(ShoppingCart cart)
     {
-        _orderService.CreateOrder(cart);
-        _logger.ForContext("ShopId", _shopId).ForContext("CartId", cart.Id).
+        orderService.CreateOrder(cart);
+        logger.ForContext("ShopId", shopId).ForContext("CartId", cart.Id).
             Information($"{_shopName}:{_division} created order");
     }
 }
