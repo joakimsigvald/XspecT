77a25c5aa5f1d38cb9a2cd8a48418460a746545e
Joakim Sigvald <joakim.sigvald@adlibris.com>
2024-07-07 20:53:56 +0200

    ...

--- Core/Spec.cs
+++ Core/Spec.cs
@@ -4,10 +4,19 @@ using XspecT.Internal.Pipelines;
 namespace XspecT;
 
 /// <summary>
-/// Base-class for specifying and executing a set of test cases for a specific method-under-test
+/// Base-class for specifying and executing a set of test cases for a specific method-under-test, with no subject-under-test and no return type
 /// </summary>
-/// <typeparam name="TSUTandResult">The return type of the method-under-test is also passed as argument to the test method</typeparam>
-public abstract class Spec<TSUTandResult> : Spec<TSUTandResult, TSUTandResult>
+public abstract class Spec : Spec<object, object>
+{
+}
+
+/// <summary>
+/// Base-class for specifying and executing a set of test cases for a specific method-under-test.
+/// This base class should typically be used for static methods, but can also be used to specify subject-under-test but no return type
+/// (or when subject-under-test and the return value has the same type)
+/// </summary>
+/// <typeparam name="TSUTorResult">The return type of the method-under-test is also passed as argument to the test method</typeparam>
+public abstract class Spec<TSUTorResult> : Spec<TSUTorResult, TSUTorResult>
 {
 }
 
