ea7083387a1bf8808c9139ab3605c80e4a33514a
Joakim Sigvald <joakim.sigvald@adlibris.com>
2024-08-03 21:36:14 +0200

    Fixed three more tests

--- Core.Test/AutoMock/WhenGivenUsingAndDefaultValue.cs
+++ Core.Test/AutoMock/WhenGivenUsingAndDefaultValue.cs
@@ -22,9 +22,18 @@ public class WhenGivenUsingAndDefaultValue : Spec<MyWrapper<string>, (string, st
 
     [Fact]
     public void ThenApplyBothAsLambdas()
-        => Given().Default(() => "ABC").And().Using(() => "DEF")
-        .When(_ => _.GetValues(A<string>()))
-        .Then().Result.Is(("DEF", "ABC"));
+    {
+        Given().Default(() => "ABC").And().Using(() => "DEF")
+            .When(_ => _.GetValues(A<string>()))
+            .Then().Result.Is(("DEF", "ABC"));
+        VerifyDescription(
+            """
+            Given using DEF
+             and default ABC
+            When GetValues(a string)
+            Then Result is ("DEF", "ABC")
+            """);
+    }
 }
 
 public class WhenGivenUsingAndDefaultModel : Spec<MyWrapper<MyModel>, (MyModel, MyModel)>
@@ -34,13 +43,31 @@ public class WhenGivenUsingAndDefaultModel : Spec<MyWrapper<MyModel>, (MyModel, 
 
     [Fact]
     public void ThenApplyBothAsValues()
-        => Given().Default(_first).And().Using(_second)
-        .When(_ => _.GetValues(A<MyModel>()))
-        .Then().Result.Is((_second, _first));
+    {
+        Given().Default(_first).And().Using(_second)
+            .When(_ => _.GetValues(A<MyModel>()))
+            .Then().Result.Is((_second, _first));
+        VerifyDescription(
+            """
+            Given default _first
+             and using _second
+            When GetValues(a MyModel)
+            Then Result is (_second, _first)
+            """);
+    }
 
     [Fact]
     public void ThenApplyBothAsLambdas()
-        => Given().Default(() => _first).And().Using(() => _second)
-        .When(_ => _.GetValues(A<MyModel>()))
-        .Then().Result.Is((_second, _first));
+    {
+        Given().Default(() => _first).And().Using(() => _second)
+            .When(_ => _.GetValues(A<MyModel>()))
+            .Then().Result.Is((_second, _first));
+        VerifyDescription(
+            """
+            Given using _second
+             and default _first
+            When GetValues(a MyModel)
+            Then Result is (_second, _first)
+            """);
+    }
 }
