1ecae4848a3606cd43c965c709fa194daca67f47
Joakim Sigvald <joakim.sigvald@adlibris.com>
2024-08-07 18:36:11 +0200

    ...

--- Core.Test/AutoMock/WhenSetupMultipleMethodsOnMock.cs
+++ Core.Test/AutoMock/WhenSetupMultipleMethodsOnMock.cs
@@ -8,7 +8,7 @@ public class WhenSetupMultipleMethodsOnMock : Spec<MyValueIntService, string>
     public void ThenSetupAll()
         => When(_ => _.GetValue(A<MyValueInt>()))
         .Given<IMyValueIntRepo>()
-        .That(_ => _.GetAsync(The<MyValueInt>())).Returns(() => A<string>())
-        .AndThat(_ => _.Get(The<MyValueInt>())).Returns(() => ASecond<string>())
+        .That(_ => _.GetAsync(The<MyValueInt>())).Returns(A<string>)
+        .AndThat(_ => _.Get(The<MyValueInt>())).Returns(ASecond<string>)
         .Then().Result.Is(TheSecond<string>());
 }
