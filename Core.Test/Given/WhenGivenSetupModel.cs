5efd280ec1978e9fe82541e341c441d8313def13
Joakim Sigvald <joakim.sigvald@adlibris.com>
2024-08-13 18:59:01 +0200

    more tests

--- Core.Test/Given/WhenGivenSetupModel.cs
+++ Core.Test/Given/WhenGivenSetupModel.cs
@@ -41,7 +41,7 @@ public class WhenGivenSetupModel : Spec<MyService, MyModel>
     {
         Given<IMyRepository>().That(_ => _.GetModels()).Returns(() => One<MyModel>())
             .When(_ => _.GetModel()) //Unspecified default value
-            .Given().A((MyModel)null)
+            .Given().A((MyModel)null) //First specific model is null
             .Then().Result.Is().NotNull(); //Not the provide null mentioned value
         VerifyDescription(
             """
--- Core.Test/Given/WhenGivenSetupModelWithDefault.cs
+++ Core.Test/Given/WhenGivenSetupModelWithDefault.cs
@@ -6,84 +6,172 @@ public class WhenGivenSetupModelWithDefault : Spec<MyService, MyModel>
 
     [Fact]
     public void GivenDefaultWithAutoMock()
-        => Given<MyModel>(_ => _.Name = _defaultName)
-        .When(_ => _.GetModel())
-        .Then().Result.Name.Is(_defaultName);
+    {
+        Given<MyModel>(_ => _.Name = _defaultName)
+            .When(_ => _.GetModel())
+            .Then().Result.Name.Is(_defaultName);
+        VerifyDescription(
+            """
+            Given MyModel { Name = _defaultName }
+            When GetModel()
+            Then Result.Name is _defaultName
+            """);
+    }
 
     [Fact]
     public void GivenDefaultNotOverridden()
-        => Given<MyModel>(_ => _.Name = _defaultName)
-        .And<IMyRepository>().That(_ => _.GetModel()).Returns(() => ASecond<MyModel>())
-        .When(_ => _.GetModel())
-        .Then().Result.Name.Is(_defaultName);
+    {
+        Given<MyModel>(_ => _.Name = _defaultName)
+            .And<IMyRepository>().That(_ => _.GetModel()).Returns(() => ASecond<MyModel>())
+            .When(_ => _.GetModel())
+            .Then().Result.Name.Is(_defaultName);
+        VerifyDescription(
+            """
+            Given MyModel { Name = _defaultName }
+             and IMyRepository.GetModel() returns a second MyModel
+            When GetModel()
+            Then Result.Name is _defaultName
+            """);
+    }
 
     [Fact]
     public void GivenTwoDefaultSetups_ThenApplySecond()
-        => Given<MyModel>(_ => _.Name = "123")
-        .Given<MyModel>(_ => _.Name = _defaultName)
-        .And<IMyRepository>().That(_ => _.GetModel()).Returns(() => ASecond<MyModel>())
-        .When(_ => _.GetModel())
-        .Then().Result.Name.Is(_defaultName);
+    {
+        Given<MyModel>(_ => _.Name = "123")
+            .Given<MyModel>(_ => _.Name = _defaultName)
+            .And<IMyRepository>().That(_ => _.GetModel()).Returns(() => ASecond<MyModel>())
+            .When(_ => _.GetModel())
+            .Then().Result.Name.Is(_defaultName);
+        VerifyDescription(
+            """
+            Given MyModel { Name = "123" }
+             and MyModel { Name = _defaultName }
+             and IMyRepository.GetModel() returns a second MyModel
+            When GetModel()
+            Then Result.Name is _defaultName
+            """);
+    }
 
     [Fact]
     public void GivenTwoDifferentDefaultSetups_ThenApplyBoth()
-        => Given<MyModel>(_ => _.Id = 123)
-        .Given<MyModel>(_ => _.Name = _defaultName)
-        .And<IMyRepository>().That(_ => _.GetModel()).Returns(() => ASecond<MyModel>())
-        .When(_ => _.GetModel())
-        .Then().Result.Name.Is(_defaultName).And(Result).Id.Is(123);
+    {
+        Given<MyModel>(_ => _.Id = 123)
+            .Given<MyModel>(_ => _.Name = _defaultName)
+            .And<IMyRepository>().That(_ => _.GetModel()).Returns(() => ASecond<MyModel>())
+            .When(_ => _.GetModel())
+            .Then().Result.Name.Is(_defaultName).And(Result).Id.Is(123);
+        VerifyDescription(
+            """
+            Given MyModel { Id = 123 }
+             and MyModel { Name = _defaultName }
+             and IMyRepository.GetModel() returns a second MyModel
+            When GetModel()
+            Then Result.Name is _defaultName
+             and Result's Id is 123
+            """);
+    }
 
     [Fact]
     public void GivenDefaultValueAndDefaultSetup()
-        => Given(_defaultName)
-        .Given<MyModel>(_ => _.Name = A<string>())
-        .And<IMyRepository>().That(_ => _.GetModel()).Returns(() => ASecond<MyModel>())
-        .When(_ => _.GetModel())
-        .Then().Result.Name.Is(_defaultName);
+    {
+        Given(_defaultName)
+            .Given<MyModel>(_ => _.Name = A<string>())
+            .And<IMyRepository>().That(_ => _.GetModel()).Returns(() => ASecond<MyModel>())
+            .When(_ => _.GetModel())
+            .Then().Result.Name.Is(_defaultName);
+        VerifyDescription(
+            """
+            Given _defaultName
+             and MyModel { Name = a string }
+             and IMyRepository.GetModel() returns a second MyModel
+            When GetModel()
+            Then Result.Name is _defaultName
+            """);
+    }
 
     [Fact]
     public void GivenDefaultIsOverridden()
-        => Given<IMyRepository>().That(_ => _.GetModel()).Returns(() => ASecond<MyModel>())
-        .When(_ => _.GetModel())
-        .Given<MyModel>(_ => _.Name = _defaultName)
-        .And().ASecond<MyModel>(_ => _.Name = "Altered")
-        .Then().Result.Name.Is("Altered");
+    {
+        Given<IMyRepository>().That(_ => _.GetModel()).Returns(() => ASecond<MyModel>())
+            .When(_ => _.GetModel())
+            .Given<MyModel>(_ => _.Name = _defaultName)
+            .And().ASecond<MyModel>(_ => _.Name = "Altered")
+            .Then().Result.Name.Is("Altered");
+        VerifyDescription(
+            """
+            Given MyModel { Name = _defaultName }
+             and a second MyModel { Name = "Altered" }
+             and IMyRepository.GetModel() returns a second MyModel
+            When GetModel()
+            Then Result.Name is "Altered"
+            """);
+    }
 
     [Fact]
     public void GivenDefaultIsNotOverridden()
-        => When(_ => MyService.Echo(A<MyModel>()))
-        .Given<IMyRepository>().That(_ => _.GetModel()).Returns(() => The<MyModel>())
-        .Given<MyModel>(_ => _.Name = _defaultName)
-        .Then().Result.Name.Is(_defaultName);
+    {
+        When(_ => MyService.Echo(A<MyModel>()))
+            .Given<IMyRepository>().That(_ => _.GetModel()).Returns(() => The<MyModel>())
+            .Given<MyModel>(_ => _.Name = _defaultName)
+            .Then().Result.Name.Is(_defaultName);
+        VerifyDescription(
+            """
+            Given MyModel { Name = _defaultName }
+             and IMyRepository.GetModel() returns the MyModel
+            When MyService.Echo(a MyModel)
+            Then Result.Name is _defaultName
+            """);
+    }
 
     [Fact]
     public void GivenDefaultIsReplaced()
-        => Given<MyModel>(_ => _.Name = _defaultName)
-        .And<IMyRepository>().That(_ => _.GetModel()).Returns(() => ASecond<MyModel>())
-        .When(_ => _.GetModel())
-        .Given().ASecond(new MyModel() { Name = "My model" })
-        .Then().Result.Name.Is("My model");
-
-    [Fact]
-    public void GivenDefaultValue_ThenIgnoreItWhenGenerateModel()
-        => Given(_defaultName)
-        .And<IMyRepository>().That(_ => _.GetModel()).Returns(() => ASecond<MyModel>())
-        .When(_ => _.GetModel())
-        .Then().Result.Name.Is(_defaultName);
+    {
+        Given<MyModel>(_ => _.Name = _defaultName)
+            .And<IMyRepository>().That(_ => _.GetModel()).Returns(() => ASecond<MyModel>())
+            .When(_ => _.GetModel())
+            .Given().ASecond(new MyModel() { Name = "My model" })
+            .Then().Result.Name.Is("My model");
+        VerifyDescription(
+            """
+            Given MyModel { Name = _defaultName }
+             and a second MyModel { new MyModel() { Name = "My model" } }
+             and IMyRepository.GetModel() returns a second MyModel
+            When GetModel()
+            Then Result.Name is "My model"
+            """);
+    }
 
     [Fact]
     public void GivenProvideDefaultSetupAfterModelIsUsedInWhen_ThenUseSetup()
-        => Given(_defaultName)
-        .And<IMyRepository>().That(_ => _.GetModel()).Returns(() => ASecond<MyModel>())
-        .When(_ => _.GetModel())
-        .Then().Result.Name.Is(_defaultName);
+    {
+        Given(_defaultName)
+            .And<IMyRepository>().That(_ => _.GetModel()).Returns(() => ASecond<MyModel>())
+            .When(_ => _.GetModel())
+            .Then().Result.Name.Is(_defaultName);
+        VerifyDescription(
+            """
+            Given _defaultName
+             and IMyRepository.GetModel() returns a second MyModel
+            When GetModel()
+            Then Result.Name is _defaultName
+            """);
+    }
 
     [Fact]
     public void GivenModel_ReferencedAsInputTwice_AndWithDefaultSetup_ThenUseDefaultSetup()
-        => When(_ => MyService.Echo(A<MyModel>()))
-        .Given<IMyRepository>().That(_ => _.SetModel(The<MyModel>())).Returns(() => Another<MyModel>())
-        .Given<MyModel>(_ => _.Id = 123)
-        .Then().Result.Id.Is(123);
+    {
+        When(_ => MyService.Echo(A<MyModel>()))
+            .Given<IMyRepository>().That(_ => _.SetModel(The<MyModel>())).Returns(() => Another<MyModel>())
+            .Given<MyModel>(_ => _.Id = 123)
+            .Then().Result.Id.Is(123);
+        VerifyDescription(
+            """
+            Given MyModel { Id = 123 }
+             and IMyRepository.SetModel(the MyModel) returns another MyModel
+            When MyService.Echo(a MyModel)
+            Then Result.Id is 123
+            """);
+    }
 }
 
 public class OverrideDefaultSetupAfterWhenReturn : Spec<MyService, MyModel>
