357c64b34ac9770c85c2f7dd04fc23338b1f4d07
Joakim Sigvald <joakim.sigvald@adlibris.com>
2025-03-04 21:41:35 +0100

    more tests

--- Core.Test/Given/WhenGivenContainingRecord.cs
+++ Core.Test/Given/WhenGivenContainingRecord.cs
@@ -6,7 +6,15 @@ namespace XspecT.Test.Given;
 public class WhenGivenContainingRecord : Spec<MyService, ContainingRecord>
 {
     [Fact]
-    public void GivenDefaultContainedInAnotherRecord_ThenUseDefault()
+    public void GivenDefaultRecordSetupContainedInAnotherRecord_ThenUseDefault()
+    {
+        Given(new MyRecord(1, A<string>()))
+            .When(_ => MyService.Echo(A<ContainingRecord>()))
+            .Then().Result.MyRecord.Name.Is(The<string>());
+    }
+
+    [Fact]
+    public void GivenDefaultRecordInstanceContainedInAnotherRecord_ThenUseDefault()
     {
         Given<MyRecord>(_ => _ with { Name = A<string>() })
             .When(_ => MyService.Echo(A<ContainingRecord>()))
