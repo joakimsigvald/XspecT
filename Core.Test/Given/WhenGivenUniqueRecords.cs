ae0b02670bc5120b06573b828dc81f1dffabac4b
Joakim Sigvald <joakim.sigvald@adlibris.com>
2025-07-23 22:03:11 +0200

    New version: Added constraint Given().Unique: All generated values of the given type will be unique

---
+++ Core.Test/Given/WhenGivenUniqueRecords.cs
@@ -0,0 +1,36 @@
+﻿using XspecT.Assert;
+
+namespace XspecT.Test.Given;
+
+public class WhenGivenUniqueRecords : Spec<MyRecord[]>
+{
+    [Fact]
+    public void WithEnoughValueSpace_ThenGenerateUniqueModelArray()
+    {
+        int range = 10;
+        When(_ => Five<MyRecord>()).Given().Default<int>(i => i % range).And().Default("Abc")
+            .And().Unique<MyRecord>()
+            .Then().Result.Is().Distinct()
+            .And.Has().All(m => m.Id >= 0 && m.Id < range);
+        Specification.Is(
+            """
+            Given int is i % range
+              and "Abc" is default
+              and all MyRecord are unique
+            When five MyRecord
+            Then Result is distinct
+                and has all m.Id >= 0 && m.Id < range
+            """);
+    }
+
+    [Fact]
+    public void WithNotEnoughValueSpace_ThenThrowSetupFailed()
+    {
+        int range = 4;
+        Xunit.Assert.Throws<SetupFailed>(() => 
+        When(_ => Five<MyRecord>()).Given().Default<int>(i => i % range).And().Default("Abc")
+            .And().Unique<MyRecord>()
+            .Then().Result.Is().Distinct()
+            .And.Has().All(m => m.Id >= 0 && m.Id < range));
+    }
+}
